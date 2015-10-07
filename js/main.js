952a4d61111c881a7bb7ce0e80e5fd64defc731b
Maryam Ali <mali62541@gmail.com>
2015-10-07 04:38:08 -0400

    done, need to slow transitions

--- js/main.js
+++ js/main.js
@@ -2,3 +2,64 @@
 
 
 // $('h4').css('background-color', red);
+// $function(){
+//   $(.'header').click(function(evt){
+//     var target=evt.target,
+//     targetPanel=target.attr('p')
+//   })
+// };
+
+
+// makes the first para disappear
+// $('h4').style.visibility='hidden';
+
+// .element {
+//     transition: [transition-property] [transition-duration] [transition-timing-function] [transition-delay];
+// }
+
+$('.slide').on('click', 'h4', function(event){
+  var newText =$(this);
+  newText.siblings().addClass('text').removeClass('closeText');
+  newText.parent().siblings().find('.text').removeClass('text').addClass('closeText');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
